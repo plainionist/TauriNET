48f004207660d58d443d0e9cc9af4144a7fa42c4
plainionist <mail@plainionist.net>
2024-11-03 18:30:01 +0100

    DotNet projects directly build to tauri-app target folder

--- build.cmd
+++ build.cmd
@@ -7,10 +7,3 @@ cd src-netcore
 dotnet build
 cd ..
 
-copy src-netcore\TauriComunication\bin\Debug\net8.0\TauriComunication.dll src-tauri\target\debug
-copy src-netcore\TauriComunication\bin\Debug\net8.0\Newtonsoft.Json.dll src-tauri\target\debug
-copy src-netcore\TauriLib\bin\Debug\net8.0\TauriIPC.dll src-tauri\target\debug
-copy src-netcore\TauriLib\bin\Debug\net8.0\TauriIPC.runtimeconfig.json src-tauri\target\debug
-
-mkdir src-tauri\target\debug\plugins
-copy src-netcore\TestApp\bin\Debug\net8.0\TestApp.plugin.dll src-tauri\target\debug\plugins
