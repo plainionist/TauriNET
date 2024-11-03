37e17de2e1512d2301c45bc8c05a5cd6918c6722
plainionist <mail@plainionist.net>
2024-11-03 22:23:48 +0100

    PluginInfo simplified

--- src-netcore/TauriDotNetBridge/PluginInfo.cs
+++ src-netcore/TauriDotNetBridge/PluginInfo.cs
@@ -5,39 +5,36 @@ namespace TauriDotNetBridge;
 
 public class PluginInfo
 {
-    public string DllPath { get; private set; }
-    public Assembly Assembly { get; private set; }
-    public Type[] Types { get; private set; }
     public List<MethodInfo> Methods { get; private set; } = new List<MethodInfo>();
     public string PluginName { get; private set; }
 
     public PluginInfo(string dllPath)
     {
-        Assembly = AppDomain.CurrentDomain.Load(LoadFile(dllPath));
-        PluginName = Assembly.GetName().Name;
+        var assembly = AppDomain.CurrentDomain.Load(LoadFile(dllPath));
+        PluginName = assembly.GetName().Name;
 
         AppDomain.CurrentDomain.AssemblyResolve += (object? sender, ResolveEventArgs args) => AssemblyDependency.AssemblyResolve(sender, args, this.PluginName);
 
-        Console.WriteLine($"Loaded dll {Path.GetFileNameWithoutExtension(dllPath)}. Name: {PluginName}");
+        Console.WriteLine($"Loaded: {Path.GetFileNameWithoutExtension(dllPath)}, Name: {PluginName}");
 
-        this.Types = this.Assembly.GetTypes();
-
-        foreach (var type in this.Types)
+        foreach (var type in assembly.GetTypes())
         {
-            var compatibleMethods = type.GetMethods(BindingFlags.Static | BindingFlags.Public).Where(m =>
-            {
-                if (m.GetCustomAttribute<RouteMethodAttribute>() == null) return false;
+            var compatibleMethods = type.GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .Where(m =>
+                {
+                    if (m.GetCustomAttribute<RouteMethodAttribute>() == null) return false;
 
-                // Verify parameters
-                ParameterInfo[] ps = m.GetParameters();
-                if (ps.Length != 1) return false;
-                if (ps[0].ParameterType.FullName != typeof(RouteRequest).FullName) return false;
+                    // Verify parameters
+                    var ps = m.GetParameters();
+                    if (ps.Length != 1) return false;
+                    if (ps[0].ParameterType.FullName != typeof(RouteRequest).FullName) return false;
 
-                // verify return
-                if (m.ReturnType.FullName != typeof(RouteResponse).FullName) return false;
+                    // verify return
+                    if (m.ReturnType.FullName != typeof(RouteResponse).FullName) return false;
 
-                return true;
-            }).ToArray();
+                    return true;
+                })
+                .ToArray();
 
             foreach (var method in compatibleMethods)
             {
@@ -57,6 +54,4 @@ public class PluginInfo
 
         return buffer;
     }
-
-    public MethodInfo? GetMethodName(string name) => Methods.FirstOrDefault(m => m.Name == name, null);
 }
