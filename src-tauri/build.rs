ae12c61a69b56297bf0bba7374e2d856476844ea
plainionist <mail@plainionist.net>
2024-11-04 21:10:46 +0100

    build script idea scripted

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,24 @@
+// use std::env;
+// use std::process::Command;
+
 fn main() {
+    // let profile = env::var("PROFILE").unwrap_or_default();
+    // if profile == "release" {
+    //     Command::new("dotnet")
+    //         .arg("build")
+    //         .arg("-c")
+    //         .arg("Release")
+    //         .arg("-p:OutputPath=../../src-tauri/target/release/dotnet")
+    //         .current_dir("../src-dotnet")
+    //         .status()
+    //         .expect("Failed to run dotnet build");
+    // } else {
+    //     Command::new("dotnet")
+    //         .arg("build")
+    //         .current_dir("../src-dotnet")
+    //         .status()
+    //         .expect("Failed to run dotnet build");
+    // }
+
     tauri_build::build()
 }
